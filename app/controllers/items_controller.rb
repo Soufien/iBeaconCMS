a79c7f050eb3e9ff7bab498230417e28ed247caf
Alberto Montagnese <albertom@slalom.com>
2014-04-03 14:58:09 -0700

    fixed kiosk view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   #before_filter :authenticate_user!
 
-  before_action :set_item, only: [:show, :edit, :update, :destroy, :embedded]
+  before_action :set_item, only: [:show, :edit, :update, :destroy, :embedded, :kiosk]
 
 
   # GET /items
