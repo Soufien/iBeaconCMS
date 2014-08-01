e4ff39b3c05f0682a107c1ccbd081118301014a5
lynnl <lynnl@slalom.com>
2014-08-01 15:20:10 -0700

    Removing columns that are unnecessary for this project

--- db/migrate/20140401200909_add_video_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddVideoToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :video, :text
-  end
-end
--- db/migrate/20140408010907_add_goodbye_content_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddGoodbyeContentToItems < ActiveRecord::Migration
-  def change
-    add_column :items, :goodbye_content, :string
-  end
-end
