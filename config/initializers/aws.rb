c8ea647a58d6fc195b3c24d44d2b3c515b4fc954
lynnl <lynnl@slalom.com>
2014-09-11 16:38:46 -0700

    ADding aws.yml

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,11 +1,12 @@
+require 'aws-sdk'
 aws_file = Rails.root.join('config', 'aws.yml')
 if (!aws_file.present?)
   AWS_CONFIG = YAML.load(File.read(aws_file))[Rails.env]
 end
-if ENV['AWS_ACCESS_KEY_ID'].present?
+if !ENV['AWS_ACCESS_KEY_ID'].present?
   AWS_CONFIG['access_key_id'] = ENV['AWS_ACCESS_KEY_ID']
 end
 
-if ENV['AWS_SECRET_KEY'].present?
+if !ENV['AWS_SECRET_KEY'].present?
   AWS_CONFIG['secret_access_key'] = ENV['AWS_SECRET_KEY']
 end
