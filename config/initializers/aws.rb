2b978ce0fb2cbe38d54b23ff41806d25da506756
lynnl <lynnl@slalom.com>
2014-09-11 17:49:53 -0700

    ADding more placeholders to aws.yml

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,12 +1,19 @@
 require 'aws-sdk'
 aws_file = Rails.root.join('config', 'aws.yml')
-if (aws_file.present?)
-  AWS_CONFIG = YAML.load(File.read(aws_file))[Rails.env]
-end
-if !ENV['AWS_ACCESS_KEY_ID'].present?
+AWS_CONFIG = YAML.load(File.read(aws_file))[Rails.env]
+
+if ENV['AWS_ACCESS_KEY_ID'].present?
   AWS_CONFIG['access_key_id'] = ENV['AWS_ACCESS_KEY_ID']
 end
 
-if !ENV['AWS_SECRET_KEY'].present?
+if ENV['AWS_SECRET_KEY'].present?
   AWS_CONFIG['secret_access_key'] = ENV['AWS_SECRET_KEY']
+end
+
+if ENV['PARAM1'].present?
+  AWS_CONFIG['region'] = ENV['PARAM1']
+end
+
+if ENV['PARAM2'].present?
+  AWS_CONFIG['bucket'] = ENV['PARAM2']
 end
